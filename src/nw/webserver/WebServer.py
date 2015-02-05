c2f353dc33a183cd18092aef08074ae198e7c9b2
Nicolas Vivien <nicolas.vivien@alcatel-lucent.com>
2015-02-05 16:33:55 +0100

    Moved web resources to web folder. Manage night-watch shutdown with server stop

--- src/nw/webserver/WebServer.py
+++ src/nw/webserver/WebServer.py
@@ -1,45 +1,54 @@
+# Copyright (c) 2015 Alcatel-Lucent Enterprise
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, RedirectHandler, Application, url
 
 import os
-import uimodules
 from logging import getLogger
-from nw.core import TaskManager
+from nw.webserver.web import uimodules
+from nw.webserver.web import Web
+from nw.webserver.api import Api
 from nw.core import NwConfiguration
+
+class Webserver:
+    def __init__(self):
+        _tornado_webserver = None
         
-class MainHandler(RequestHandler):
-    def get(self):
-        self.render("index.html", 
-                    Tasks = TaskManager.getTasks())
+    def make_app(self):
+        return Application(
+            handlers = [
+                url(r"/", RedirectHandler,dict(url=r"/index.html")),
+                url(r"/index.html", Web.MainHandler),
+                url(r"/status.html", Web.StatusHandler),
+                url(r"/task.html", Web.TaskHandler),
+            ],
+            template_path = os.path.join(os.path.dirname(__file__), "web/templates"),
+            static_path = os.path.join(os.path.dirname(__file__), "web/static"),
+            ui_modules = uimodules,
+            debug=NwConfiguration.getNwConfiguration().webserver_debug
+        )
 
-class StatusHandler(RequestHandler):
-    def get(self):
-        self.render("status.html", 
-                    Tasks = TaskManager.getTasks())
+    def start(self):
+        getLogger(__name__).debug("Initializing Night Watch's WebServer...")
+        application = self.make_app()
+        application.listen(NwConfiguration.getNwConfiguration().webserver_port)
+        self._tornado_webserver = IOLoop.instance()
+        getLogger(__name__).info("Start Night Watch's WebServer")
+        self._tornado_webserver.start()
 
-class TaskHandler(RequestHandler):
-    def get(self):
-        task_name = self.get_argument('task_name')
-        getLogger(__name__).info('Get task "{}"'.format(task_name))
-        self.render("task.html", 
-                    Task = TaskManager.getTasks().get(task_name))
-    
-def make_app():
-    return Application(
-        handlers = [
-            url(r"/", RedirectHandler,dict(url=r"/index.html")),
-            url(r"/index.html", MainHandler),
-            url(r"/status.html", StatusHandler),
-            url(r"/task.html", TaskHandler),
-        ],
-        template_path = os.path.join(os.path.dirname(__file__), "templates"),
-        static_path = os.path.join(os.path.dirname(__file__), "static"),
-        ui_modules = uimodules,
-        debug=NwConfiguration.getNwConfiguration().webserver_debug
-    )
-
-def start():
-    getLogger(__name__).info("Start Night Watch's WebServer")
-    application = make_app()
-    application.listen(NwConfiguration.getNwConfiguration().webserver_port)
-    IOLoop.instance().start()
+    def stop(self):
+        getLogger(__name__).info("Stop Night Watch's WebServer")
+        self._tornado_webserver.stop()
--- src/nw/webserver/uimodules.py
+++
@@ -1,6 +0,0 @@
-from tornado.web import UIModule
-
-class Task(UIModule):
-    def render(self, task, show_config = False):
-        return self.render_string(
-            "uitemplates/module-task.html", task=task, show_config=show_config)
